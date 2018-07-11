529867c465eec5b486c0d663ec6d2fde448a7eaf
Palmeto <Palmeto@DESKTOP-2R1O2ST>
2018-07-11 17:06:56 +0530

    Revert "Initial Draft"
    This reverts commit 5533a14aec8b4753056ca2982a41fceea8c5f69c.

--- AzureQueueStorage/Program.cs
+++ AzureQueueStorage/Program.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Azure;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
 
 namespace AzureQueueStorage
 {
