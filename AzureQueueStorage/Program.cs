117268e494da48c30e0776931684665894307df7
Palmeto <Palmeto@DESKTOP-2R1O2ST>
2018-07-11 17:08:40 +0530

    initial code

--- AzureQueueStorage/Program.cs
+++ AzureQueueStorage/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace AzureQueueStorage
 {
